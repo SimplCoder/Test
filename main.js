897fa3e6dc67e9ea55553efc0985c164497423f4
Rupesh Dhadiwal <dhadiwalrupesh7@gmail.com>
2022-05-25 11:58:07 +0400

    update Main.js
    added space utility and calculated length of print

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-const myArgs = process.argv.slice(2)
-console.info(myArgs[0]);
+
 class CharPattern
 {
+	// this is added to create space of given length
+	addSpace(n)
+	{
+		for (var i = 0; i < n; ++i)
+		{
+			process.stdout.write(" ");
+		}
+	}
 	
 }
 
-function main()
-{
-	var pattern = new CharPattern();
-	
+function main(char)
+{ 
+    var startAscii= char.charCodeAt(0);
+    const asciiA= 65;   //ascii of Char A
+    var pattern = new CharPattern();
+    const length= startAscii-asciiA+1;  //calculating the legth
+		
 }
-main();
+const myArgs = process.argv.slice(2)
+main(myArgs[0]);
